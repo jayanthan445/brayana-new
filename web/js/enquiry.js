32d37906e32489d1f1dd44540cb92799c88116a0
pitchaipandiyan <pitchaipandiyan.89@gmail.com>
2018-07-09 00:26:05 +0530

    Added UI design for enquiry notification and commission entry

---
+++ web/js/enquiry.js
@@ -0,0 +1,12 @@
+function openEnquiryAddPage(){
+    window.location='./enquiryAddEdit.html';
+}
+
+function openCommisionUpdatePage(){
+    window.location='./comissionEdit.html';
+}
+
+
+$(document).ready(function(){
+         
+});
